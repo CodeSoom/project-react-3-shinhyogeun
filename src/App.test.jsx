59ca3fee43f17b8e34d31ff1c700e7b691909dd4
shinhyogeun <tls1gy2rms3@naver.com>
2021-03-23 15:48:06 +0900

    App test 수정

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+
+import { useDispatch, useSelector } from 'react-redux';
+
 import { render } from '@testing-library/react';
 
 import App from './App';
 
+jest.mock('react-redux');
+
 test('App', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+  useSelector.mockImplementation((selector) => selector({
+    input: '새로운 노래',
+  }));
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   const { queryByText } = render(<App />);
 
   expect(queryByText('My PlayList')).toBeInTheDocument();
