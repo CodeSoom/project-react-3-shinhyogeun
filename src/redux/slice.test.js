1195036e61994b14beda0f1662e90e03e7a49112
shinhyogeun <tls1gy2rms3@naver.com>
2021-03-24 17:15:11 +0900

    API를 이용한 fetch 기능 구현
    fetch를 한 후에 상태까지 update합니다

--- src/redux/slice.test.js
+++ src/redux/slice.test.js
@@ -1,4 +1,13 @@
-import reducer, { updateInput } from './slice';
+import thunk from 'redux-thunk';
+
+import configureStore from 'redux-mock-store';
+
+import reducer, { searchMusic, setMusics, updateInput } from './slice';
+
+jest.mock('../services/api');
+
+const middleware = [thunk];
+const mockStore = configureStore(middleware);
 
 describe('slice', () => {
   describe('reducer', () => {
@@ -10,6 +19,24 @@ describe('slice', () => {
       const state = reducer(initialState, updateInput('새로운 음악'));
 
       expect(state.input).toBe('새로운 음악');
+    });
+  });
+
+  describe('actions', () => {
+    let store;
+    describe('fetchYoutubeMusics', () => {
+      beforeEach(() => {
+        store = mockStore({
+          input: 'DEAN',
+        });
+      });
+
+      it('Youtube 음악을 불러와 setMusics를 실행한다.', async () => {
+        await store.dispatch(searchMusic());
+
+        const actions = store.getActions();
+        expect(actions[0]).toEqual(setMusics([]));
+      });
     });
   });
 });
