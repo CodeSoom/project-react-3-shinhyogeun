6dfaf515a7abae3edf940397b6ae73aee4f276a8
shinhyogeun <tls1gy2rms3@naver.com>
2021-03-26 18:43:48 +0900

    배포를 고려한 Router Path 수정

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,13 @@ export default function App() {
     <>
       <header>
         <h1>
-          <Link to="/">My Playlist</Link>
+          <Link to="/project-react-3-shinhyogeun/">My Playlist</Link>
         </h1>
       </header>
       <SearchFormPage />
       <Switch>
         {/* <Route exact path="/" component={} /> */}
-        <Route exact path="/search/:keyword" component={SearchResultPage} />
+        <Route exact path="/project-react-3-shinhyogeun/search/:keyword" component={SearchResultPage} />
       </Switch>
     </>
   );
