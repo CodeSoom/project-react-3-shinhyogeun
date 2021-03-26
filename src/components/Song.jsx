33b322ae5b157a7827156da0ddf8ee2adb50a84a
shinhyogeun <tls1gy2rms3@naver.com>
2021-03-26 16:31:02 +0900

    구조분해 줄바꿈

--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -3,7 +3,16 @@ import React from 'react';
 import YouTube from '@u-wave/react-youtube';
 
 export default function Song({ music }) {
-  const { id: { videoId }, snippet: { title, thumbnails: { high: { url } } } } = music;
+  const {
+    id: { videoId },
+    snippet: {
+      title,
+      thumbnails: {
+        high: { url },
+      },
+    },
+  } = music;
+
   return (
     <li>
       <img src={url} alt="thumbnails" />
