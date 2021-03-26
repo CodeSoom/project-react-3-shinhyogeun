16144c5e1ac0e5cfe31ecff2322d762b8e14493f
shinhyogeun <tls1gy2rms3@naver.com>
2021-03-26 11:22:47 +0900

    배포 준비
    1.ci.yml 생성
    2.gh-pages 설치 : https://www.npmjs.com/package/gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,12 @@
-import YouTube from '@u-wave/react-youtube';
-
 import React from 'react';
-
-import { useSelector } from 'react-redux';
 
 import SearchFormContainer from './container/SearchFormContainer';
 
 export default function App() {
-  const { musics } = useSelector((state) => ({
-    musics: state.musics,
-  }));
   return (
     <>
       <h1>My PlayList</h1>
       <SearchFormContainer />
-      <ul>
-        {musics?.map(({ id: { videoId } }, index) => (
-          <li key={String(index)}><YouTube video={videoId} /></li>
-        ))}
-      </ul>
     </>
   );
 }
