313e02506563222d47d9a53bd750d5ef12984012
shinhyogeun <tls1gy2rms3@naver.com>
2021-03-26 16:39:20 +0900

    App에 SearchFormResult로Route연결

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 
+import { Switch, Route } from 'react-router-dom';
 import SearchFormContainer from './container/SearchFormContainer';
+import SearchResultPage from './pages/SearchResultPage';
 
 export default function App() {
   return (
     <>
       <h1>My PlayList</h1>
       <SearchFormContainer />
+      <Switch>
+        {/* <Route exact path="/playlist" component={} /> */}
+        <Route exact path="/:keyword" component={SearchResultPage} />
+      </Switch>
     </>
   );
 }
