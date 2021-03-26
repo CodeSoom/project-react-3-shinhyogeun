86a97f7c7cc2e7504065591d8f929f50297b7978
shinhyogeun <tls1gy2rms3@naver.com>
2021-03-26 22:06:23 +0900

    baseURL 추가

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,13 @@ export default function App() {
     <>
       <header>
         <h1>
-          <Link to="/project-react-3-shinhyogeun/">My Playlist</Link>
+          <Link to="/">My Playlist</Link>
         </h1>
       </header>
       <SearchFormPage />
       <Switch>
         {/* <Route exact path="/" component={} /> */}
-        <Route exact path="/project-react-3-shinhyogeun/search/:keyword" component={SearchResultPage} />
+        <Route exact path="/search/:keyword" component={SearchResultPage} />
       </Switch>
     </>
   );
