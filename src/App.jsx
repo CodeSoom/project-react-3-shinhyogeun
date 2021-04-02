76446e758f899fb9e0f08c83d7e8629e841170a4
shinhyogeun <tls1gy2rms3@naver.com>
2021-04-03 00:39:47 +0900

    global styling과 App의 헤더부분 스타일링

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,37 @@ import { useDispatch } from 'react-redux';
 
 import { Switch, Route, Link } from 'react-router-dom';
 
+import styled from '@emotion/styled';
 import PlayerPage from './pages/PlayerPage';
 import PlaylistPage from './pages/PlaylistPage';
 import SearchFormPage from './pages/SearchFormPage';
 import SearchResultPage from './pages/SearchResultPage';
 
+import GlobalStyle from './styles/global';
+
 import { appendPlaylistMusic } from './redux/slice';
 
 import { loadItem } from './services/storage';
+
+const Title = styled.h1({
+  fontFamily: 'Montserrat, sans-serif',
+  fontSize: '50px',
+  textAlign: 'center',
+  height: '65px',
+  borderBottom: '5px solid teal',
+  borderTop: '5px solid red',
+  '& a:link, a:visited, a:hover': {
+    color: 'black',
+    textDecoration: 'none',
+  },
+});
+
+const Header = styled.header({
+  padding: '0 30px',
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+});
 
 export default function App() {
   const dispatch = useDispatch();
@@ -21,16 +44,17 @@ export default function App() {
 
   return (
     <>
-      <header>
-        <h1>
+      <Header>
+        <Title>
           <Link to="/">My Playlist</Link>
-        </h1>
-      </header>
-      <SearchFormPage />
+        </Title>
+        <SearchFormPage />
+      </Header>
       <Switch>
         <Route exact path="/" component={PlaylistPage} />
         <Route path="/result/:searchQuery?" component={SearchResultPage} />
       </Switch>
+      <GlobalStyle />
       <PlayerPage />
     </>
   );
