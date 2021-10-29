873bf9165f465f4e7aabee2cf3017c7b4a70105c
shinhyogeun <tls1gy2rms3@naver.com>
2021-10-30 01:25:54 +0900

    rollback

--- src/index.jsx
+++ src/index.jsx
@@ -12,7 +12,7 @@ import App from './App';
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename="/p">
       <App />
     </BrowserRouter>
   </Provider>,
